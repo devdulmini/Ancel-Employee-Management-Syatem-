980f07477b5147a7cb3b285e752069cfba227ca6
Dulmini Hasinika <dulminiwalisundara17@gmail.com>
2022-12-15 22:59:58 +0530

    first commit

---
+++ src/payment/db.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payment;
+
+import Manager.*;
+import com.employee.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author just
+ */
+public class db {
+    
+     static Connection dbnnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/ancel_employee","root","");
+            
+        return con;
+        
+        
+        }
+        catch(Exception e){
+             JOptionPane.showMessageDialog(null, e);
+             return null;
+    }
+    
+}
+}
